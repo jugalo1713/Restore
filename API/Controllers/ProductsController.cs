6a14071e38c1a56f3c6630e8700a25f77c73662d
JulianGalloNexer <julian.gallo@convatec.com>
2025-01-13 23:19:01 -0500

    products controller created

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(StoreContext _context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductsAsync()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductByIdAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            return product != null ? 
+                Ok(product)
+                : NotFound($"Content with Id {id} not found");
+        }
+    }
+}
